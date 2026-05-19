daf4b322659cd52a777bae2fd5ef91b54ad8ee96
bjordanborns-ux <bjordanborns@gmail.com>
2026-05-19 01:28:01 -0400

    Added orbit state where acceleration does not change
    Need to fix: Telemetry and ascent pop up doubling after liftoff, "orbitude", formatting errors.

--- testground.py
+++ testground.py
@@ -1,10 +1,6 @@
 import time
 import random
 print("NorthLight Launch (Function sytem)")
-  
-# Telemetry Functions:
-
-Altitude = ()
 
 # Functions
 def weather_check():
@@ -20,13 +16,13 @@ def weather_check():
     print("Weather is No-Go. Hold Launch.")
     time.sleep(1)
     print("System Terminating.")
-
+# Gets called from weather
 def launch_approved():
     print("Weather systems verified, approved for launch.")
     time.sleep(1)
-    print("Initating countdown sequence. T-")
+    print("Initating launch countdown sequence.")
     random_failure()
-
+# Failure set to occur if random number picked is between 90 and 100. Will launch successfully if failure occurs. 
 def random_failure():
    failure = (random.randint (0, 100))
    if failure > 90 and failure < 100:
@@ -35,19 +31,20 @@ def random_failure():
       time.sleep(1)
       mission_state = "Failed."
       print("Mission Status:", mission_state)
+      SystemExit
    elif failure > 0 and failure < 90: 
       countdown()
 
 countdown_list = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 def countdown():
     mission_state = "Countdown"
-    print("Mission Status:", mission_state)
+    print("Mission Status:", mission_state, "T -")
     for i in range(0, 10, 1): 
         countdown_list[i]
         time.sleep(1)
         print (countdown_list[i])    
     liftoff()
-
+# Defines the launch time for telemetry_timer to utilize when forming MET. 
 def liftofftime():
     launch_time = time.time()
     return launch_time
@@ -60,13 +57,36 @@ def liftoff():
     mission_state = "Ascent" 
     print("Mission Status:", mission_state)
     liftofftime()
-
-def mission_timer():
-    
+    return mission_state
+# Acceleration (unrealistic #) causes velocity to increase which causes altitude_tel to increase. 
+def telemetry_timer_ascent(altitude, fuel, velocity):
     current_time = time.time()
     mission_time = (current_time - liftoff_time)
-    print("MET: T+", convert(mission_time), end='\r')
+    acceleration = int (.6 * 60)
+    velocity = int (0 + (acceleration * mission_time))
+    altitude_tel = int (altitude + (velocity * mission_time))
+    altitude = altitude_tel
+    fuel_burn = acceleration * .05
+    fuel_new = int (fuel - fuel_burn)
+    fuel = fuel_new
+    print("MET: T+", convert(mission_time), "Altitude:" , altitude_tel, "Velocity:", velocity, "Fuel:", fuel, end='\r')
+    return altitude, fuel, velocity 
 
+# Switches to 0 acceleration which causes velocity, and altitude to slow/stop directly. Occurs when mission state is Orbit.
+def telemetry_timer_orbit(altitude, fuel, velocity):
+    current_time = time.time()
+    mission_time = (current_time - liftoff_time)
+    acceleration = 0
+    velocity = int (0 + (acceleration * mission_time))
+    altitude_tel = int (altitude + (velocity * mission_time))
+    altitude = altitude_tel
+    fuel_burn = acceleration * .05
+    fuel_new = int (fuel - fuel_burn)
+    fuel = fuel_new
+    print("MET: T+", convert(mission_time), "Altitude:" , altitude_tel, "Velocity:", velocity, "Fuel:", fuel, end='\r')
+    return altitude, fuel, velocity 
+
+# Converts MET to readable form instead of default. 
 def convert(mission_time):
     sec = mission_time
     min, sec = divmod(sec, 60)
@@ -75,12 +95,25 @@ def convert(mission_time):
 
 # Start of script running (outside of definitions and functions)
 weather_check()
+# Starting values for telemetry system
+mission_state = "Checking Weather"
 liftoff_time = liftofftime()
-mission_state = "Ascent"
+altitude = 0 
+fuel = 100 
+velocity = 0
+altitude, fuel, velocity = telemetry_timer_ascent(altitude, fuel, velocity)
+mission_state = liftoff()
+
 while mission_state == "Ascent":
-   mission_timer()
+   altitude, fuel, velocity = telemetry_timer_ascent(altitude, fuel, velocity)
    time.sleep(1)
+   if altitude <= 10000:
+    telemetry_timer_ascent(altitude, fuel, velocity)
+   elif altitude >= 10000:
+    mission_state = "Orbit"
+    print("Mission Status:", mission_state, end='\n')
 
-
-
+while mission_state == "Orbit":
+   telemetry_timer_orbit(altitude, fuel, velocity)
+   time.sleep(1)
 
